ca2cd625ac2faabf49fb36eda9dfa4e301d606c8
Boris <marc.chevallier@ulb.be>
2025-12-19 11:13:49 +0100

    update

--- model.py
+++ model.py
@@ -23,12 +23,14 @@ class Solver:
             # variables onle dependant on t 
             v.id("side", t)
             v.id("DEP", t)
-            v.id("ARR", t)
             v.id("ALL", t)
 
             for item in D.items():
                 # pas sure
+                d = item[1]
                 v.id("dur", t, item[1]) 
+                v.id("ARR", t - d)
+
             
             for p in p:
                 for s in S : 
@@ -41,6 +43,13 @@ class Solver:
         self.cnf.append([v.id("DEP"), v.id("dep")])
 
         # ARR_{t} = 1
+        self.cnf.append([v.id("ARR"), v.id("dur")])
+
+        # ALL_{t} = 1
+        self.cnf.append([v.id("ALL"), v.id("B")])
+
+        # dur_{t, d} = 1
+        self.cnf.append([-v.id("dep")])
 
 
 
