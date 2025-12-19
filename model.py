9e0741199c850135e01cfaeb267d04db7d285b38
Boris <marc.chevallier@ulb.be>
2025-12-20 00:01:09 +0100

    a corriger

--- model.py
+++ model.py
@@ -78,6 +78,55 @@ class Solver:
             if found_eligible_p:
                 self.cnf.append(clause_phi5)
 
+            # if boat on side A and B at time t, then all departures at time t must be from side A or side B
+            for t in range(0, T):
+                for p in range(0, P):
+                    for item in self.D.items():
+                        p, d = item
+                        side_t = v.id(("side", t))
+                        dur_t_d = v.id(("dur", t, d))
+
+                        # Arrives in A
+                        b_dep_t_p_retour = v.id(("dep", t, p, "r"))
+                        # Arrives in B
+                        a_dep_t_p_aller  = v.id(("dep", t, p, "a"))
+
+                        if t + d <= T:
+                            arr_t = v.id(("ARR", t + d))
+                            side_t_d = v.id(("side", t + d))
+
+                            A_p_t = v.id(("A", t + d, p))
+                            B_p_t = v.id(("B", t + d, p))
+
+                            # todo : revoir
+
+                            # B_dep_t_p_retour AND dur_t_d  -> ( arr_t AND side_t_d AND A_p_t AND side_t)
+                            condition = [-b_dep_t_p_retour, -dur_t_d]
+                            self.cnf.append([condition + [-arr_t]])
+                            self.cnf.append([condition + [side_t_d]]) # Force Side 0 (B)
+                            self.cnf.append([condition + [B_p_t]])
+                            self.cnf.append([condition + [side_t]])
+
+                            combined_right_b = [arr_t, -side_t_d, A_p_t, -side_t]
+                            self.cnf.append(combined_right_b + [b_dep_t_p_retour])
+                            self.cnf.append(combined_right_b + [dur_t_d])
+
+                            # A_dep_t_p_aller AND dur_t_d  -> ( arr_t AND side_t_d AND B_p_t AND side_t)
+                            condition = [-a_dep_t_p_aller, -dur_t_d]
+                            self.cnf.append([condition + [arr_t]])
+                            self.cnf.append([condition + [side_t_d]])
+                            self.cnf.append([condition + [B_p_t]])
+                            self.cnf.append([condition + [-side_t]])
+
+                            combined_right_a = [-arr_t, -side_t_d, A_p_t, side_t]
+                            self.cnf.append(combined_right_a + [a_dep_t_p_aller])
+                            self.cnf.append(combined_right_a + [dur_t_d])
+                  
+
+
+                    
+              
+
         
         
         # A -> B                        
